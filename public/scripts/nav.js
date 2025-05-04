770b71ad0cb1b1960a0914e5d1642da477ec0887
mattglbrt <matt@everyway.io>
2025-05-04 15:50:45 -0400

    created basic mobile navigation

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -23,3 +23,12 @@ window.addEventListener('scroll', () => {
   }
   lastScroll = currentScroll;
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const menuButton = document.getElementById('mobile-menu-button');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  menuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+  });
+});
