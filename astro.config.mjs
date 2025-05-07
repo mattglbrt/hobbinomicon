91169f6700c7ea15a2bb60d994046a8a6767fb6d
mattglbrt <matt@goeveryway.com>
2025-05-07 14:21:11 -0400

    feat: blog added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify";
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: "https://hobbinomicon.com",
@@ -10,5 +11,6 @@ export default defineConfig({
   integrations: [
     tailwind(),
     sitemap(),
+    react(),
   ],
 });
