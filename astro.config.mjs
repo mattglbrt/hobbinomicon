845212d2846201c231372b985415326755c6ea59
mattglbrt <matt@goeveryway.com>
2025-11-29 09:57:02 -0500

    SEO, Accessability, and 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hobbinomicon.com',
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), sitemap()],
 });
